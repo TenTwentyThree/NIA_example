a912e305205df63d7771aef6f68f9b87e38d5eb1
Johannes Schrumpf <jschrumpf@uos.de>
2022-04-21 13:06:31 +0200

    integrated config file into experiment process

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 SIMULATED_ANNEALING = {
-    'SearchSpace':{
-        'num_households' : 150,
+    'SearchSpace': {
+        'multiple': True,
+        'range': [100, 500, 1000, 5000],
+        'num_households': 150,
         },
-    'Annealing':{
-        'delta_temp':500,
-        'energy_factor':0.85,
+    'Annealing': {
+        'delta_temp': 500,
+        'energy_factor': 0.85,
+    },
+    'Experiment': {
+        'iterations': [10, 50, 200, 500, 1000, 5000, 10000]
     }
 }
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import numpy as np
+from config import SIMULATED_ANNEALING as cnfg
 import pandas as pd
 import logging
 from data import generate_searchspace as generator
@@ -8,18 +8,18 @@ from src import optimizers, visualize_results
 def execute():
     logging.getLogger('Main').setLevel(logging.INFO)
     annealer = optimizers.SimulatedAnnealing()
-    for households in [100,500,1000,5000]:
-        generator.model_power_distribution(num_households=households,save=True,save_prefix=f'{households}_')
+    if cnfg['SearchSpace']['multiple']:
+        household_range = cnfg['SearchSpace']['range']
+    else:
+        household_range = [cnfg['SearchSpace']['num_households']]
+    for households in household_range:
+        generator.model_power_distribution(num_households=households, save=True, save_prefix=f'{households}_')
         annealer._set_new_search_space(prefix=f'{households}_')
         logging.info(f'Maximum score: {annealer.max_inhabitants}')
-        history = [annealer.delta_anneal(delta_temp=item,cold_start=True,label=item)[0] for item in [10,
-                                                                                                     50,
-                                                                                                     200,
-                                                                                                     500,
-                                                                                                     1000,
-                                                                                                     5000,
-                                                                                                     10000]]
+        history = [annealer.delta_anneal(delta_temp=item, cold_start=True, label=item)[0]
+                   for item in cnfg['Experiment']['iterations']]
         visualize_results.visualize_total_score(pd.concat(history), households)
 
+
 if __name__ == '__main__':
-    execute()
+    execute()  # depending on your os, you may need to call this function from python console to make it work
